8b1f1f0b84baab3033dbdcf0a049cb264290a69d
youssefali_3001 <youssef.sls.2020@gmail.com>
2023-06-04 15:02:51 +0300

    Add Redirection after login

---
+++ main.js
@@ -0,0 +1,6 @@
+let url = window.location.href;
+if (url.includes("Years=second") && url.includes("Term=second")) {
+  window.open("source.html", "_self");
+} else {
+  window.open("arabic/login/u3_p2_t2.html", "_self");
+}
